af72e9e4a00e9e6cd15813ed2da143732d3f838d
hikim <hikim@bzsys.co.kr>
2022-05-25 15:17:27 +0900

    feat: 초기 화면 세팅중

--- src/main/java/kr/co/bzsys/study/controller/HomeController.java
+++ src/main/java/kr/co/bzsys/study/controller/HomeController.java
@@ -1,15 +1,19 @@
 package kr.co.bzsys.study.controller;
 
+import java.time.LocalDateTime;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 public class HomeController {
 
-  @GetMapping("/home")
-  public String homepage() {
-    System.out.println("ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ");
+  @RequestMapping(value="/home")
+  public String requestToday(Model model) {
+    model.addAttribute("today", LocalDateTime.now());
+    log.info("model" + model);
     return "home";
   }
-
 }
---
+++ src/main/resources/static/home.js
@@ -0,0 +1,1 @@
+window.console.log("옴옴옴");
