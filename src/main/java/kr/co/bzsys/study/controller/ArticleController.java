be1313a0460c6cb27739053416f8a0bf7b14da14
leehyun <leehyun@bzsys.co.kr>
2022-05-26 16:57:25 +0900

    컨트롤러, html 추가
    ArticleController
    templates/articles/new.html

---
+++ src/main/java/kr/co/bzsys/study/controller/ArticleController.java
@@ -0,0 +1,15 @@
+package kr.co.bzsys.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ArticleController {
+
+  @GetMapping("/articles/new")
+  public String newArticleForm() {
+    return "articles/new";
+  }
+
+}
