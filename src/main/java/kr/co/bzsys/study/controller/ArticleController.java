938783e7b225491253876842e85f88f7fad86682
hikim <hikim@bzsys.co.kr>
2022-07-25 16:47:15 +0900

    feat: controller service 연결

--- src/main/java/kr/co/bzsys/study/controller/ArticleController.java
+++ src/main/java/kr/co/bzsys/study/controller/ArticleController.java
@@ -3,6 +3,7 @@ package kr.co.bzsys.study.controller;
 import java.util.List;
 import kr.co.bzsys.study.dto.Board;
 import kr.co.bzsys.study.mapper.BoardMapper;
+import kr.co.bzsys.study.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +15,15 @@ public class ArticleController {
   @Autowired
   BoardMapper boardMapper;
 
+  @Autowired
+  BoardService boardService;
+
   @GetMapping("/test")
   public List<Board> creatArticle() {
-    return boardMapper.selectBoard();
+
+    List<Board> boardList = boardService.selectBoard();
+
+    return boardList;
   }
 
 }
